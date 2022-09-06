321ee18a41e553a413c48a9b361a872bd5330e5a
ValentinaZabal <106683196+ValentinaZabal@users.noreply.github.com>
2022-09-06 15:32:33 -0300

    Add files via upload

---
+++ page/nosotras/nosotras.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nosotras',
+  templateUrl: './nosotras.page.html',
+  styleUrls: ['./nosotras.page.scss'],
+})
+export class NosotrasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
